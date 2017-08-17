3309b238a6bc04722fd7505cf06258a42143a218
willys7 <wilfuentes0320@gmail.com>
2017-08-16 23:23:48 -0600

    call getColumns api first test

--- js/repositories/entitiesRepository.js
+++ js/repositories/entitiesRepository.js
@@ -32,6 +32,26 @@
                 return err;
             });
         }
+
+        function getColumns(id){
+            return $http({
+
+                "method": "GET", 
+                "url": "http://localhost:3000/v1/column/getColumnByEntity",
+                "data" : {},
+                "params" : {
+                    id = id
+                },
+                "headers": {
+                }
+            }).then(function (response) {
+                return response.data;
+            })
+            .catch(function (err) {
+                console.log(err);
+                return err;
+            });
+        }
     }
 
 })();
